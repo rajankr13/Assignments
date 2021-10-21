db665be8acdf71796dbf74b604c60e0a30dfcae9
rajankr13 <89027993+rajankr13@users.noreply.github.com>
2021-10-21 12:07:38 +0530

    Add files via upload

---
+++ interview questions.py
@@ -0,0 +1,20 @@
+1.What is the difference between list and tuples in Python?
+2.What are the key features of Python?
+3.What are Dict and List comprehensions?
+4.What are the common built-in data types in Python?
+5.What are Keywords in Python?
+6.What is slicing in Python?
+7.What is a dictionary in Python?
+8.How will you convert a string to all lowercase?
+9.What does len() do?
+10.What are the common built-in data types in Python?
+11.How to add values to a python array?
+12.How to remove values to a python array?
+13.What are lists and tuples? What is the key difference between the two?
+14.Explain split() and join() functions in Python?
+15.What are negative indexes and why are they used?
+16.How will you get the items that are not common to both the given series A and B?
+17.What is lambda in Python?
+18.What is the difference between xrange and range?
+19.What is the use of // operator in Python?
+20.In Python what are iterators?
